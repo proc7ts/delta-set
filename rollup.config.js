d59a390a717b971e53ea5a5a568ce6b28b756401
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-11-23 13:16:55 +0700

    Generate type definitions with `@proc7ts/rollup-plugin-flat-dts`

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
+import flatDts from '@proc7ts/rollup-plugin-flat-dts';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import ts from '@wessberg/rollup-plugin-ts';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
-import pkg from './package.json';
 
 export default {
+  input: {
+    'delta-set': './src/index.ts',
+  },
   plugins: [
     commonjs(),
     ts({
@@ -22,17 +25,23 @@ export default {
     nodeResolve(),
     sourcemaps(),
   ],
-  input: './src/index.ts',
   output: [
     {
-      file: pkg.main,
+      dir: 'dist',
       format: 'cjs',
       sourcemap: true,
+      entryFileNames: '[name].cjs',
     },
     {
-      file: pkg.module,
+      dir: '.',
       format: 'esm',
       sourcemap: true,
+      entryFileNames: 'dist/[name].js',
+      plugins: [
+        flatDts({
+          tsconfig: 'tsconfig.main.json',
+        }),
+      ],
     },
   ],
 };
