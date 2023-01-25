1aec211d94f2e9e12b26255ea171d74421423d22
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-25 09:06:21 +0700

    Build with `@run-z/project-config`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  ignorePatterns: ['dist/', 'target/', 'd.ts/', '*.d.ts'],
+  ignorePatterns: ['dist/', 'target/'],
   extends: ['@run-z'],
   overrides: [
     {
@@ -23,7 +23,7 @@ module.exports = {
       extends: ['@run-z/eslint-config/jest'],
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: './tsconfig.spec.json',
+        project: './tsconfig.json',
       },
     },
   ],
